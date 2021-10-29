74422e1be36927ac9fbeb6c733a644df424f01c8
Dima Pristupa <dima.pristupa@pm.me>
2021-10-29 13:41:51 +0300

    🚧 Finish 3.2.

--- 03_chat/src/main.rs
+++ 03_chat/src/main.rs
@@ -20,4 +20,27 @@
 //    3.8. Handling Disconnection
 //         ^ https://book.async.rs/tutorial/handling_disconnection.html
 
-fn main() {}
+use async_std::{
+    net::{TcpListener, ToSocketAddrs}, // 3
+    prelude::*,                        // 1
+    task,                              // 2
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
+        // TODO
+    }
+
+    Ok(())
+}
+
+// main
+fn main() -> Result<()> {
+    let fut = accept_loop("127.0.0.1:8080");
+
+    task::block_on(fut)
+}
