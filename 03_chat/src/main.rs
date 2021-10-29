e42ebc73d8bcf3a2d50a2bdbad2db2f0de17d18f
Dima Pristupa <dima.pristupa@pm.me>
2021-10-29 17:01:30 +0300

    🚧 Finish 3.3

--- 03_chat/src/main.rs
+++ 03_chat/src/main.rs
@@ -21,24 +21,63 @@
 //         ^ https://book.async.rs/tutorial/handling_disconnection.html
 
 use async_std::{
-    net::{TcpListener, ToSocketAddrs}, // 3
-    prelude::*,                        // 1
-    task,                              // 2
+    io::BufReader,
+    net::{TcpListener, TcpStream, ToSocketAddrs},
+    prelude::*,
+    task,
 };
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+where
+    F: Future<Output = Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e)
+        }
+    })
+}
+
 async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     let mut incoming = listener.incoming();
+
     while let Some(stream) = incoming.next().await {
-        // TODO
+        let stream = stream?;
+        println!("Accepting from: {}", stream.peer_addr()?);
+        let _handle = task::spawn(connection_loop(stream)); // 1
     }
 
     Ok(())
 }
 
-// main
+async fn connection_loop(stream: TcpStream) -> Result<()> {
+    let reader = BufReader::new(&stream); // 2
+    let mut lines = reader.lines();
+
+    let name = match lines.next().await {
+        None => Err("peer disconnected immediately")?,
+        Some(line) => line?,
+    };
+    println!("name = {}", name);
+
+    while let Some(line) = lines.next().await {
+        let line = line?;
+        let (dest, msg) = match line.find(':') {
+            None => continue,
+            Some(idx) => (&line[..idx], line[idx + 1..].trim()),
+        };
+        let dest: Vec<String> = dest
+            .split(',')
+            .map(|name| name.trim().to_string())
+            .collect();
+        let msg: String = msg.to_string();
+    }
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let fut = accept_loop("127.0.0.1:8080");
 
