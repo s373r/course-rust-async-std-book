d750eba1716ad936390d9c58c501421c91686111
Dima Pristupa <dima.pristupa@pm.me>
2021-10-29 13:34:54 +0300

    🚧 Finish 3.1.

---
+++ 03_chat/src/main.rs
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// --- Index ---
+// 3. Tutorial: Implementing a chat
+//         ^ https://book.async.rs/tutorial/index.html
+//    3.1. Specification and Getting started
+//         ^ https://book.async.rs/tutorial/specification.html
+//    3.2. Writing an Accept Loop
+//         ^ https://book.async.rs/tutorial/accept_loop.html
+//    3.3. Receiving Messages
+//         ^ https://book.async.rs/tutorial/receiving_messages.html
+//    3.4. Sending Messages
+//         ^ https://book.async.rs/tutorial/sending_messages.html
+//    3.5. Connecting Readers and Writers
+//         ^ https://book.async.rs/tutorial/connecting_readers_and_writers.html
+//    3.6. All Together
+//         ^ https://book.async.rs/tutorial/all_together.html
+//    3.7. Clean Shutdown
+//         ^ https://book.async.rs/tutorial/clean_shutdown.html
+//    3.8. Handling Disconnection
+//         ^ https://book.async.rs/tutorial/handling_disconnection.html
+
+fn main() {}
